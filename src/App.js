3c6f62b97776c9f63a54ec53a38ff6ab99b5d594
Beste Burcu Bayhan <bestebbayhan@gmail.com>
2020-10-06 18:02:26 +0300

    Add signIn and signOut functions using firebase library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 import firebase from "firebase/app";
 import "firebase/firestore";
@@ -9,14 +11,39 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
-  // your config
+  apiKey: "AIzaSyD47qezbm6vLoWOfLNYp3vMpVH2OhPU2XA",
+  authDomain: "chat-room-91dd4.firebaseapp.com",
+  databaseURL: "https://chat-room-91dd4.firebaseio.com",
+  projectId: "chat-room-91dd4",
+  storageBucket: "chat-room-91dd4.appspot.com",
+  messagingSenderId: "689361786892",
+  appId: "1:689361786892:web:1ae7af9a7d67af5a13b629",
+  measurementId: "G-P5261BQDMF",
 });
 
-function App() {
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <Button onClick={signInWithGoogle}>Sign in with Google</Button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <Button onClick={() => auth.signOut()}>Sign Out</Button>
+  );
+}
+
+function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header></header>
+      <section>{user ? "" : <SignIn />}</section>
     </div>
   );
 }
